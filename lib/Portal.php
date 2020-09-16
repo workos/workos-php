d151bf1f2d7165e7bf02e09a29862eaf4b70b8c8
Will Duffy <willman@workos.com>
2020-09-16 13:30:37 -0400

    Add generateLink() to \WorkOS\Portal (#14)
    * Adds an SDK method to generate an Admin Portal link.

--- lib/Portal.php
+++ lib/Portal.php
@@ -33,6 +33,30 @@ class Portal
     }
 
     /**
+     * Generate a Portal Link scoped to an Organization.
+     *
+     * @param string $organization An Organization identifier.
+     * @param string $intent The intent of the Admin Portal. Possible values are ["sso"].
+     * @param null|string $returnUrl The URL to which WorkOS should send users when they click on
+     *      the link to return to your website. (Optional).
+     *
+     * @return \WorkOS\Resource\PortalLink
+     */
+    public function generateLink($organization, $intent, $returnUrl = null)
+    {
+        $generateLinkPath = "portal/generate_link";
+        $params = [
+            "organization" => $organization,
+            "intent" => $intent,
+            "return_url" => $returnUrl
+        ];
+
+        $response = Client::request(Client::METHOD_POST, $generateLinkPath, null, $params, true);
+
+        return Resource\PortalLink::constructFromResponse($response);
+    }
+
+    /**
      * List Organizations.
      *
      * @param null|array $domain Filter organizations to only return those that are associated with
