2769c81a9792cc26c96001fa07f3cda7916d5c2f
Sheldon Vaughn <sheldon@workos.com>
2021-11-05 12:36:35 -0600

    Add webhook validation methods (#65)
    * Add webhook validation methods to PHP SDK

---
+++ lib/Webhook.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace WorkOS;
+
+/**
+ * Class Webhook.
+ *
+ * This class includes functions for users to pass in a webhook header/body and receive
+ * the webhook ID, body, and event type if the webhook is valid/secure otherwise an error
+ * indictating the issue.
+ */
+
+class Webhook
+{
+   
+    /** Initializes an Event object from a JSON payload
+     * @param \WorkOS\Resource\Webhook
+     *
+     * @return boolean true
+     */
+
+    public function constructEvent($sigHeader, $payload, $secret, $tolerance)
+    {
+        $eventResult = $this->verifyHeader($sigHeader, $payload, $secret, $tolerance);
+
+        if ($eventResult == "pass"):
+            return Resource\Webhook::constructFromPayload($payload); else:
+            return $eventResult;
+        endif;
+    }
+
+    /**
+     *  verifyHeader verifies the header returned from WorkOS contains a valid timestamp
+     *  no older than 3 minutes, and computes the signature.
+     * @param $sigheader is the WorkOS header containing v1 signature and timestamp
+     * @param $payload is the body of the webhook
+     * @param $secret is the webhook secret from the WorkOS dashboard
+     * @param $tolerance is the number of seconds old the webhook can be before it's invalid
+     * @return boolean true
+     */
+
+    private function verifyHeader($sigHeader, $payload, $secret, $tolerance)
+    {
+        $timestamp = (int)$this->getTimeStamp($sigHeader);
+        $signature = $this->getSignature($sigHeader);
+
+        $currentTime = time();
+        $decodedBody = utf8_decode($payload);
+        $signedPayload = $timestamp . "." . $decodedBody;
+        $expectedSignature = hash_hmac("sha256", $signedPayload, $secret, false);
+
+        if (empty($timestamp)):
+            return "No Timestamp available"; elseif (empty($signature)):
+            return "No signature hash found with expected scheme v1"; elseif ($timestamp < $currentTime - $tolerance):
+            return "Timestamp outside of tolerance"; elseif ($signature != $expectedSignature):
+            return "Constructed signature " . $expectedSignature . "Does not match WorkOS Header Signature " . $signature; else:
+            return "pass";
+        endif;
+    }
+
+    /**
+    *  Splits WorkOS header's two values and pulls out timestamp value and returns it
+    * @param $sigheader is the WorkOS header containing v1 signature and timestamp
+    * @return $timestamp
+    */
+
+    private function getTimeStamp($sigHeader)
+    {
+        $workosHeadersSplit = explode(',', $sigHeader, 2);
+        $timestamp = substr($workosHeadersSplit[0], 2);
+        return $timestamp;
+    }
+    
+    /**
+    * splits WorkOS headers two values and pulls out the signature value and returns it
+    * @param $sigheader is the WorkOS header containing v1 signature and timestamp
+    * @return $signature
+    */
+
+    private function getSignature($sigHeader)
+    {
+        $workosHeadersSplit = explode(',', $sigHeader, 2);
+        $signature = substr($workosHeadersSplit[1], 4);
+        return $signature;
+    }
+}
---
+++ tests/WorkOS/WebhookTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace WorkOS;
+
+class WebhookTest extends \PHPUnit\Framework\TestCase
+{
+    use TestHelper {
+        setUp as protected traitSetUp;
+    }
+
+    protected function setUp()
+    {
+        $this->traitSetUp();
+
+        $this->withApiKey();
+        $this->ap = new Webhook();
+    }
+
+    public function testConstructEventWebhook()
+    {
+        $time = time();
+        $payload = '{"id":"wh_01FGCG6SDYCT5XWZT9CDW0XEB8","data":{"id":"conn_01EHWNC0FCBHZ3BJ7EGKYXK0E6","name":"Foo Corp\'s Connection","state":"active","object":"connection","domains":[{"id":"conn_domain_01EHWNFTAFCF3CQAE5A9Q0P1YB","domain":"foo-corp.com","object":"connection_domain"}],"connection_type":"OktaSAML","organization_id":"org_01EHWNCE74X7JSDV0X3SZ3KJNY"},"event":"connection.activated"}';
+        $secret = 'secret';
+        $tolerance = 180;
+        $decodedBody = utf8_decode($payload);
+        $signedPayload = $time . "." . $decodedBody;
+        $expectedSignature = hash_hmac("sha256", $signedPayload, $secret, false);
+        $headers = 't=' . $time . ', v1=' . $expectedSignature;
+        $result = $this->generateConnectionFixture();
+
+        $expectation = '{"id":"wh_01FGCG6SDYCT5XWZT9CDW0XEB8","data":{"id":"conn_01EHWNC0FCBHZ3BJ7EGKYXK0E6","name":"Foo Corp\'s Connection","state":"active","object":"connection","domains":[{"id":"conn_domain_01EHWNFTAFCF3CQAE5A9Q0P1YB","domain":"foo-corp.com","object":"connection_domain"}],"connection_type":"OktaSAML","organization_id":"org_01EHWNCE74X7JSDV0X3SZ3KJNY"},"event":"connection.activated"}';
+        
+        $response = $this->ap->constructEvent($headers, $payload, $secret, $tolerance);
+        $this->assertSame($expectation, json_encode($response));
+    }
+
+    // Fixtures
+
+    private function generateConnectionFixture()
+    {
+        return json_encode([
+            "id" => "conn_01E0CG2C820RP4VS50PRJF8YPX",
+            "domains" => [
+              [
+                "id" => "conn_dom_01E2GCC7Q3KCNEFA2BW9MXR4T5",
+                "domain" => "workos.com"
+              ]
+            ],
+            "state" => "active",
+            "status" => "linked",
+            "name" => "Google OAuth 2.0",
+            "connectionType" => "GoogleOAuth",
+            "organizationId" => "org_1234",
+        ]);
+    }
+}
