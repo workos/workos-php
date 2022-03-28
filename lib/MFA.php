00d8b7e62190e2e238d91ae3768e0f1f69f762a2
Sheldon Vaughn <sheldonvaughn@icloud.com>
2022-03-28 09:26:45 -0600

    Add MFA and MFA Unit Tests (#87)
    * Add MFA and MFA Unit Tests
    
    * Ran PHP linter
    
    * Updated unit tests
    
    * fix typos in comments
    
    * Added in additional error handling and updated unit test
    
    * Fixed unit testing, dropped change from VerificationChallenge resource
    
    Co-authored-by: Sheldon Vaughn <svaughn@sheldons-mbp.lan>

---
+++ lib/MFA.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace WorkOS;
+
+/**
+ * Class MFA.
+ *
+ * This class facilitates the use of WorkOS MFA.
+ */
+class MFA
+{
+    /**
+     * Enrolls a new Authentication Factor
+     *
+     * @param string $type - Type of factor to be enrolled (sms or totp)
+     * @param null|string $totpIssuer - Name of the Organization
+     * @param null|string $totpUser - Email of user
+     * @param null|string $phoneNumber - Phone number of user
+     */
+
+    public function enrollFactor(
+        $type,
+        $totpIssuer = null,
+        $totpUser = null,
+        $phoneNumber = null
+    ) {
+        $enrollPath = "auth/factors/enroll";
+
+        if (!isset($type)) {
+            $msg = "Incomplete arguments: Need to specify a type of factor";
+            throw new Exception\UnexpectedValueException($msg);
+        }
+
+        if ($type != "sms" && $type != "totp") {
+            $msg = "Type Parameter must either be 'sms' or 'totp'";
+            throw new Exception\UnexpectedValueException($msg);
+        }
+
+        if ($type == "sms" && !isset($phoneNumber)) {
+            $msg = "Incomplete arguments: phoneNumber needs to be specified when using 'sms' as type.";
+            throw new Exception\UnexpectedValueException($msg);
+        }
+
+        if ($type == "totp" && (!isset($totpIssuer) || !isset($totpUser))) {
+            $msg = "Incomplete arguments: totpIssuer and totpUser need to be specified when using 'totp' as type.";
+            throw new Exception\UnexpectedValueException($msg);
+        }
+
+        $params = [
+        "type" => $type,
+        "totp_issuer" => $totpIssuer,
+        "totp_user" => $totpUser,
+        "phone_number" => $phoneNumber
+    ];
+        $response = Client::request(
+            Client::METHOD_POST,
+            $enrollPath,
+            null,
+            $params,
+            true
+        );
+
+        if ($type == "totp") {
+            return Resource\AuthenticationFactorTotp::constructFromResponse($response);
+        } elseif ($type == "sms") {
+            return Resource\AuthenticationFactorSms::constructFromResponse($response);
+        }
+    }
+
+
+    /**
+     * Initiates the authentication process (a challenge) for an authentication factor
+     *
+     * @param string $authenticationFactorId - ID of the authentication factor
+     * @param string|null $smsTemplate - Optional parameter to customize the message for sms type factors. Must include "{{code}}" if used.
+    */
+
+    public function challengeFactor(
+        $authenticationFactorId,
+        $smsTemplate = null
+    ) {
+        $challengePath = "auth/factors/challenge";
+
+        if (!isset($authenticationFactorId)) {
+            $msg = "Incomplete arguments: 'authentication_factor_id' is a required parameter";
+            throw new Exception\UnexpectedValueException($msg);
+        }
+
+        $params = [
+        "authentication_factor_id" => $authenticationFactorId,
+        "sms_template" => $smsTemplate
+    ];
+
+        $response = Client::request(
+            Client::METHOD_POST,
+            $challengePath,
+            null,
+            $params,
+            true
+        );
+
+        return Resource\AuthenticationChallenge::constructFromResponse($response);
+    }
+
+    /**
+     * Verifies the one time password provided by the end-user.
+     *
+     * @param string $authenticationChallengeId - The ID of the authentication challenge that provided the user the verification code.
+     * @param string $code - The verification code sent to and provided by the end user.
+    */
+
+    public function verifyFactor(
+        $authenticationChallengeId,
+        $code
+    ) {
+        $verifyPath = "auth/factors/verify";
+
+        if (!isset($authenticationChallengeId) || !isset($code)) {
+            $msg = "Incomplete arguments: 'authenticationChallengeId' and 'code' are required parameters";
+            throw new Exception\UnexpectedValueException($msg);
+        }
+
+        $params = [
+        "authentication_challenge_id" => $authenticationChallengeId,
+        "code" => $code
+    ];
+
+        $response = Client::request(
+            Client::METHOD_POST,
+            $verifyPath,
+            null,
+            $params,
+            true
+        );
+
+        return Resource\VerificationChallenge::constructFromResponse($response);
+    }
+}
---
+++ lib/Resource/AuthenticationFactorTotp.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WorkOS\Resource;
+
+/**
+ * Class AuthenticationFactorTotp.
+ */
+class AuthenticationFactorTotp extends BaseWorkOSResource
+{
+    public const RESOURCE_TYPE = "authentication_factor";
+
+    public const RESOURCE_ATTRIBUTES = [
+        "object",
+        "id",
+        "createdAt",
+        "updatedAt",
+        "type",
+        "environmentId",
+        "totp"
+    ];
+
+    public const RESPONSE_TO_RESOURCE_KEY = [
+        "object" => "object",
+        "id" => "id",
+        "created_at" => "createdAt",
+        "updated_at" => "updatedAt",
+        "type" => "type",
+        "environment_id" => "environmentId",
+        "totp" => "totp"
+    ];
+}
