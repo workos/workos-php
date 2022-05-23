1e9f13ff0041b830aa45e827c777894e3835864f
Erik Engervall <erik.engervall@gmail.com>
2022-05-23 17:05:15 +0200

    Remove environment_id fields

--- lib/Resource/AuthenticationFactorSms.php
+++ lib/Resource/AuthenticationFactorSms.php
@@ -15,7 +15,6 @@ class AuthenticationFactorSms extends BaseWorkOSResource
         "createdAt",
         "updatedAt",
         "type",
-        "environmentId",
         "sms"
     ];
 
@@ -25,7 +24,6 @@ class AuthenticationFactorSms extends BaseWorkOSResource
         "created_at" => "createdAt",
         "updated_at" => "updatedAt",
         "type" => "type",
-        "environment_id" => "environmentId",
         "sms" => "sms"
     ];
 }
