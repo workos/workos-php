c6e56361fd0a51dd6349161ee9191aec9e15f73e
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2023-03-30 16:22:22 +0200

    Fix ConnectionType usage in phpdoc (#140)
    * Fix ConnectionType usage in phpdoc
    
    * Remove phpstan phpdoc

--- lib/SSO.php
+++ lib/SSO.php
@@ -15,7 +15,7 @@ class SSO
      * @param null|string $domain Domain of the user that will be going through SSO
      * @param null|string $redirectUri URI to direct the user to upon successful completion of SSO
      * @param null|array $state Associative array containing state that will be returned from WorkOS as a json encoded string
-     * @param null|\WorkOS\Resource\ConnectionType $provider Service provider that handles the identity of the user
+     * @param null|string $provider Service provider that handles the identity of the user
      * @param null|string $connection Unique identifier for a WorkOS Connection
      * @param null|string $organization Unique identifier for a WorkOS Organization
      * @param null|string $domainHint DDomain hint that will be passed as a parameter to the IdP login page
@@ -192,7 +192,7 @@ class SSO
      * List Connections.
      *
      * @param null|string $domain Domain of a Connection
-     * @param null|\WorkOS\Resource\ConnectionType $connectionType Authentication service provider descriptor
+     * @param null|string $connectionType Authentication service provider descriptor
      * @param null|string $organizationId Organization ID of the Connection(s)
      * @param int $limit Maximum number of records to return
      * @param null|string $before Connection ID to look before
