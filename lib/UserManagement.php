16e4db58e5589273b3999f1dbef48953cd6032ab
Sheldon Vaughn <sheldon@workos.com>
2023-08-16 15:04:00 -0600

    Add `listUsers` and `createUser` functions (#157)
    * Add `listUsers` and `createUser` functions

--- lib/UserManagement.php
+++ lib/UserManagement.php
@@ -7,6 +7,8 @@ namespace WorkOS;
  */
 class UserManagement
 {
+    public const DEFAULT_PAGE_SIZE = 10;
+
     /**
      * Add a user to an organization.
      *
@@ -75,6 +77,91 @@ class UserManagement
         $usersPath = "users/{$userId}";
 
         $response = Client::request(Client::METHOD_GET, $usersPath, null, null, true);
+
+        return Resource\User::constructFromResponse($response);
+    }
+
+    /**
+     * List Users.
+     *
+     * @param null|string $type User type "unmanaged" or "managed"
+     * @param null|string $email
+     * @param null|string $organization Organization users are a member of
+     * @param int $limit Maximum number of records to return
+     * @param null|string $before User ID to look before
+     * @param null|string $after User ID to look after
+     * @param \WorkOS\Resource\Order $order The Order in which to paginate records
+     *
+     * @throws Exception\WorkOSException
+     *
+     * @return array An array containing the following:
+     *      null|string User ID to use as before cursor
+     *      null|string User ID to use as after cursor
+     *      array \WorkOS\Resource\User instances
+     */
+    public function listUsers(
+        $type = null,
+        $email = null,
+        $organization = null,
+        $limit = self::DEFAULT_PAGE_SIZE,
+        $before = null,
+        $after = null,
+        $order = null
+    ) {
+        $usersPath = "users";
+        $params = [
+            "type" => $type,
+            "email" => $email,
+            "organization" => $organization,
+            "limit" => $limit,
+            "before" => $before,
+            "after" => $after,
+            "order" => $order
+        ];
+
+        $response = Client::request(
+            Client::METHOD_GET,
+            $usersPath,
+            null,
+            $params,
+            true
+        );
+
+        $users = [];
+        list($before, $after) = Util\Request::parsePaginationArgs($response);
+        foreach ($response["data"] as $responseData) {
+            \array_push($users, Resource\User::constructFromResponse($responseData));
+        }
+
+        return [$before, $after, $users];
+    }
+
+    /**
+     * Create User.
+     *
+     * @param string $email The name of the User.
+     * @param string|null $password The name of the User.
+     * @param string|null $firstName The name of the User.
+     * @param string|null $lastName The name of the User.
+     * @param boolean|null $emailVerified The name of the User.
+     * @param string|null $idempotencyKey is a unique string that identifies a distinct user
+     * @throws Exception\WorkOSException
+     *
+     * @return \WorkOS\Resource\User
+     */
+    public function createUser($email, $password, $firstName, $lastName, $emailVerified, $idempotencyKey = null)
+    {
+        $idempotencyKey ? $headers = array("Idempotency-Key: $idempotencyKey") : $headers = null;
+        $usersPath = "users";
+        $params = [
+            "email" => $email,
+            "password" => $password,
+            "first_name" => $firstName,
+            "last_name" => $lastName,
+            "email_verified" => $emailVerified
+        ];
+
+        $response = Client::request(Client::METHOD_POST, $usersPath, $headers, $params, true);
 
         return Resource\User::constructFromResponse($response);
     }
---
+++ tests/WorkOS/UserManagementTest.php
@@ -0,0 +1,261 @@
+<?php
+
+namespace WorkOS;
+
+class UserManagementTest extends \PHPUnit\Framework\TestCase
+{
+    use TestHelper {
+        setUp as traitSetUp;
+    }
+
+    protected function setUp(): void
+    {
+        $this->traitSetUp();
+
+        $this->withApiKeyAndClientId();
+        $this->userManagement = new UserManagement();
+    }
+
+    public function testAddUserToOrganization()
+    {
+        $userId = "user_01H7X1M4TZJN5N4HG4XXMA1234";
+        $usersOrganizationsPath = "users/{$userId}/organizations";
+
+        $result = $this->addUserToOrganizationResponseFixture();
+
+        $params = [
+            "organization_id" => "org_01EHQMYV6MBK39QC5PZXHY59C3",
+        ];
+
+        $this->mockRequest(
+            Client::METHOD_POST,
+            $usersOrganizationsPath,
+            null,
+            $params,
+            true,
+            $result
+        );
+
+        $user = $this->userFixture();
+
+        $response = $this->userManagement->addUserToOrganization("user_01H7X1M4TZJN5N4HG4XXMA1234", "org_01EHQMYV6MBK39QC5PZXHY59C3");
+        $this->assertSame($user, $response->toArray());
+    }
+
+    public function testCreateUser()
+    {
+        $usersPath = "users";
+
+        $result = $this->createUserResponseFixture();
+
+        $params = [
+            "email" => "test@test.com",
+            "password" => "x^T!V23UN1@V",
+            "first_name" => "Damien",
+            "last_name" => "Alabaster",
+            "email_verified" => true
+        ];
+
+        $this->mockRequest(
+            Client::METHOD_POST,
+            $usersPath,
+            null,
+            $params,
+            true,
+            $result
+        );
+
+        $user = $this->userFixture();
+
+        $response = $this->userManagement->createUser("test@test.com", "x^T!V23UN1@V", "Damien", "Alabaster", true);
+        $this->assertSame($user, $response->toArray());
+    }
+
+
+    public function testGetUser()
+    {
+        $userId = "user_01H7X1M4TZJN5N4HG4XXMA1234";
+        $usersPath = "users/{$userId}";
+
+        $result = $this->getUserResponseFixture();
+
+        $this->mockRequest(
+            Client::METHOD_GET,
+            $usersPath,
+            null,
+            null,
+            true,
+            $result
+        );
+
+        $user = $this->userFixture();
+
+        $response = $this->userManagement->getUser($userId);
+
+        $this->assertSame($user, $response->toArray());
+    }
+
+    public function testListUsers()
+    {
+        $usersPath = "users";
+        $params = [
+            "type" => null,
+            "email" => null,
+            "organization" => null,
+            "limit" => UserManagement::DEFAULT_PAGE_SIZE,
+            "before" => null,
+            "after" => null,
+            "order" => null
+        ];
+
+        $result = $this->listUsersResponseFixture();
+
+        $this->mockRequest(
+            Client::METHOD_GET,
+            $usersPath,
+            null,
+            $params,
+            true,
+            $result
+        );
+
+        $user = $this->userFixture();
+        list($before, $after, $users) = $this->userManagement->listUsers();
+        $this->assertSame($user, $users[0]->toArray());
+    }
+
+    public function testRemoveUserFromOrganization()
+    {
+        $userId = "user_01H7X1M4TZJN5N4HG4XXMA1234";
+        $organizationId = "org_01EHQMYV6MBK39QC5PZXHY59C3";
+        $usersOrganizationsDeletionPath = "users/{$userId}/organizations/{$organizationId}";
+
+        $result = $this->removeUserFromOrganizationResponseFixture();
+
+        $this->mockRequest(
+            Client::METHOD_DELETE,
+            $usersOrganizationsDeletionPath,
+            null,
+            null,
+            true,
+            $result
+        );
+
+        $user = $this->userFixture();
+
+        $response = $this->userManagement->removeUserFromOrganization("user_01H7X1M4TZJN5N4HG4XXMA1234", "org_01EHQMYV6MBK39QC5PZXHY59C3");
+        $this->assertSame($user, $response->toArray());
+    }
+
+    // Fixtures
+
+    private function addUserToOrganizationResponseFixture()
+    {
+        return json_encode([
+            "object" => "user",
+            "id" => "user_01H7X1M4TZJN5N4HG4XXMA1234",
+            "user_type" => "unmanaged",
+            "email" => "test@test.com",
+            "first_name" => "Damien",
+            "last_name" => "Alabaster",
+            "email_verified_at" => "2021-07-25T19:07:33.155Z",
+            "sso_profile_id" => "1AO5ZPQDE43",
+            "google_oauth_profile_id" => "goog_123ABC",
+            "created_at" => "2021-06-25T19:07:33.155Z",
+            "updated_at" => "2021-06-25T19:07:33.155Z"
+        ]);
+    }
+
+    private function createUserResponseFixture()
+    {
+        return json_encode([
+            "object" => "user",
+            "id" => "user_01H7X1M4TZJN5N4HG4XXMA1234",
+            "user_type" => "unmanaged",
+            "email" => "test@test.com",
+            "first_name" => "Damien",
+            "last_name" => "Alabaster",
+            "email_verified_at" => "2021-07-25T19:07:33.155Z",
+            "sso_profile_id" => "1AO5ZPQDE43",
+            "google_oauth_profile_id" => "goog_123ABC",
+            "created_at" => "2021-06-25T19:07:33.155Z",
+            "updated_at" => "2021-06-25T19:07:33.155Z"
+        ]);
+    }
+
+    private function getUserResponseFixture()
+    {
+        return json_encode([
+            "object" => "user",
+            "id" => "user_01H7X1M4TZJN5N4HG4XXMA1234",
+            "user_type" => "unmanaged",
+            "email" => "test@test.com",
+            "first_name" => "Damien",
+            "last_name" => "Alabaster",
+            "email_verified_at" => "2021-07-25T19:07:33.155Z",
+            "sso_profile_id" => "1AO5ZPQDE43",
+            "google_oauth_profile_id" => "goog_123ABC",
+            "created_at" => "2021-06-25T19:07:33.155Z",
+            "updated_at" => "2021-06-25T19:07:33.155Z"
+        ]);
+    }
+
+    private function listUsersResponseFixture()
+    {
+        return json_encode([
+            "data" => [
+                [
+                    "object" => "user",
+                    "id" => "user_01H7X1M4TZJN5N4HG4XXMA1234",
+                    "user_type" => "unmanaged",
+                    "email" => "test@test.com",
+                    "first_name" => "Damien",
+                    "last_name" => "Alabaster",
+                    "email_verified_at" => "2021-07-25T19:07:33.155Z",
+                    "sso_profile_id" => "1AO5ZPQDE43",
+                    "google_oauth_profile_id" => "goog_123ABC",
+                    "created_at" => "2021-06-25T19:07:33.155Z",
+                    "updated_at" => "2021-06-25T19:07:33.155Z"
+                ]
+            ],
+            "list_metadata" => [
+                "before" => null,
+                "after" => null
+            ],
+        ]);
+    }
+
+    private function removeUserFromOrganizationResponseFixture()
+    {
+        return json_encode([
+            "object" => "user",
+            "id" => "user_01H7X1M4TZJN5N4HG4XXMA1234",
+            "user_type" => "unmanaged",
+            "email" => "test@test.com",
+            "first_name" => "Damien",
+            "last_name" => "Alabaster",
+            "email_verified_at" => "2021-07-25T19:07:33.155Z",
+            "sso_profile_id" => "1AO5ZPQDE43",
+            "google_oauth_profile_id" => "goog_123ABC",
+            "created_at" => "2021-06-25T19:07:33.155Z",
+            "updated_at" => "2021-06-25T19:07:33.155Z"
+        ]);
+    }
+
+    private function userFixture()
+    {
+        return [
+            "object" => "user",
+            "id" => "user_01H7X1M4TZJN5N4HG4XXMA1234",
+            "userType" => "unmanaged",
+            "email" => "test@test.com",
+            "firstName" => "Damien",
+            "lastName" => "Alabaster",
+            "emailVerifiedAt" => "2021-07-25T19:07:33.155Z",
+            "googleOauthProfileId" => "goog_123ABC",
+            "ssoProfileId" => "1AO5ZPQDE43",
+            "createdAt" => "2021-06-25T19:07:33.155Z",
+            "updatedAt" => "2021-06-25T19:07:33.155Z"
+        ];
+    }
+}
