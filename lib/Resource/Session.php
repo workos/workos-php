8d9e8818f38ee04060daa04bef8b019ecd176bb8
Sheldon Vaughn <sheldon@workos.com>
2023-08-22 14:54:11 -0600

    Add `authenticateUserWith*`  functions (#163)
    * Add authenticateUser functions

---
+++ lib/Resource/Session.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WorkOS\Resource;
+
+/**
+ * Class Session.
+ *
+ * @property string $id
+ * @property string $created_at
+ * @property string $expires_at
+ * @property string $token
+ * @property string $authorized_organizations
+ * @property string $unauthorized_organizations
+ */
+class Session extends BaseWorkOSResource
+{
+    public const RESOURCE_TYPE = "session";
+
+    public const RESOURCE_ATTRIBUTES = [
+        "object",
+        "id",
+        "token",
+        "authorizedOrganizations",
+        "unauthorizedOrganizations",
+        "createdAt",
+        "expiresAt"
+    ];
+
+    public const RESPONSE_TO_RESOURCE_KEY = [
+        "object" => "object",
+        "id" => "id",
+        "token" => "token",
+        "authorized_organizations" => "authorizedOrganizations",
+        "unauthorized_organizations" => "unauthorizedOrganizations",
+        "created_at" => "createdAt",
+        "expires_at" => "expiresAt"
+    ];
+}
