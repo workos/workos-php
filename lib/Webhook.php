8988129c13234bb898161baabb5b96addc4b7e70
Braden Keith <bkeith@romegadigital.com>
2025-03-11 13:16:09 -0400

    Enhance Webhook class with improved code style and PHPDoc annotations

--- lib/Webhook.php
+++ lib/Webhook.php
@@ -20,7 +20,7 @@ class Webhook
     {
         $eventResult = $this->verifyHeader($sigHeader, $payload, $secret, $tolerance);
 
-        if ($eventResult == "pass") {
+        if ($eventResult == 'pass') {
             return Resource\Webhook::constructFromPayload($payload);
         } else {
             return $eventResult;
@@ -31,45 +31,43 @@ class Webhook
      * Verifies the header returned from WorkOS contains a valid timestamp
      * no older than 3 minutes, and computes the signature.
      *
-     * @param string $sigHeader WorkOS header containing v1 signature and timestamp
-     * @param string $payload Body of the webhook
-     * @param string $secret Webhook secret from the WorkOS dashboard
-     * @param int $tolerance Number of seconds old the webhook can be before it's invalid
-     *
+     * @param  string  $sigHeader  WorkOS header containing v1 signature and timestamp
+     * @param  string  $payload  Body of the webhook
+     * @param  string  $secret  Webhook secret from the WorkOS dashboard
+     * @param  int  $tolerance  Number of seconds old the webhook can be before it's invalid
      * @return bool true
      */
     public function verifyHeader($sigHeader, $payload, $secret, $tolerance)
     {
-        $timestamp = (int)$this->getTimeStamp($sigHeader);
+        $timestamp = (int) $this->getTimeStamp($sigHeader);
         $signature = $this->getSignature($sigHeader);
 
         $currentTime = time();
-        $signedPayload = $timestamp . "." . $payload;
-        $expectedSignature = hash_hmac("sha256", $signedPayload, $secret, false);
+        $signedPayload = $timestamp.'.'.$payload;
+        $expectedSignature = hash_hmac('sha256', $signedPayload, $secret, false);
 
         if (empty($timestamp)) {
-            return "No Timestamp available";
+            return 'No Timestamp available';
         } elseif (empty($signature)) {
-            return "No signature hash found with expected scheme v1";
+            return 'No signature hash found with expected scheme v1';
         } elseif ($timestamp < $currentTime - $tolerance) {
-            return "Timestamp outside of tolerance";
+            return 'Timestamp outside of tolerance';
         } elseif ($signature != $expectedSignature) {
-            return "Constructed signature " . $expectedSignature . "Does not match WorkOS Header Signature " . $signature;
+            return 'Constructed signature '.$expectedSignature.'Does not match WorkOS Header Signature '.$signature;
         } else {
-            return "pass";
+            return 'pass';
         }
     }
 
     /**
      * Splits WorkOS header's two values and pulls out timestamp value and returns it
      *
-     * @param string $sigHeader WorkOS header containing v1 signature and timestamp
-     *
+     * @param  string  $sigHeader  WorkOS header containing v1 signature and timestamp
      * @return $timestamp
      */
     public function getTimeStamp($sigHeader)
     {
-        $workosHeadersSplit = explode(",", $sigHeader, 2);
+        $workosHeadersSplit = explode(',', $sigHeader, 2);
         $timestamp = substr($workosHeadersSplit[0], 2);
 
         return $timestamp;
@@ -78,13 +76,12 @@ class Webhook
     /**
      * Splits WorkOS headers two values and pulls out the signature value and returns it
      *
-     * @param string $sigHeader WorkOS header containing v1 signature and timestamp
-     *
+     * @param  string  $sigHeader  WorkOS header containing v1 signature and timestamp
      * @return string
      */
     public function getSignature($sigHeader)
     {
-        $workosHeadersSplit = explode(",", $sigHeader, 2);
+        $workosHeadersSplit = explode(',', $sigHeader, 2);
         $signature = substr($workosHeadersSplit[1], 4);
 
         return $signature;
