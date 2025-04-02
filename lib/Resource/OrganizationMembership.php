0c57b3574a679366bae37e8d5ea9edac56a4465e
Braden Keith <bradenkeith@gmail.com>
2025-04-02 16:16:06 -0400

    IDE & Typehint improvements (#264)
    * Improve Webhook and BaseWorkOSResource PHPDoc types
    
    * Enhance Webhook class with improved code style and PHPDoc annotations
    
    * Add accessToken and refreshToken to AuthenticationResponse class
    
    * Update AuthenticationResponse class to include organizationId as nullable, and add accessToken, refreshToken, and impersonator properties
    
    * Enhance OrganizationMembership class with additional PHPDoc properties for improved type documentation
    
    * Add metadata and external id (#268)
    
    And allow to be passed when creating or updating a user or organization.
    
    * Add email standard attribute to DirectoryUser and mark deprecated standard attributes (#261)
    
    * Add function to get organization by external id (#270)
    
    And fix typo in getOrganization docstring
    
    * Add support for creating, getting and updating users with external_id property (#267)
    
    Co-authored-by: Eric Roberts <ericroberts@gmail.com>
    
    * Bump to version 4.22.0. (#269)
    
    * Structured responses to webhook events (#265)
    
    * Add WebhookResponse class for handling webhook actions and responses
    
    * Refactor WebhookResponse create method and improve validation
    
    * Resolve linting error
    
    ---------
    
    Co-authored-by: Braden Keith <bkeith@romegadigital.com>
    
    * Update deprecation notices in DirectoryUser class to include version information and improve clarity
    
    * Update deprecation notices in Organizations class to include version information and improve formatting
    
    * Update doc blocks for deprecation notices
    
    * Update tests to expect Role Slug
    
    ---------
    
    Co-authored-by: Braden Keith <bkeith@romegadigital.com>
    Co-authored-by: Eric Roberts <ericroberts@gmail.com>
    Co-authored-by: Matt Dzwonczyk <9063128+mattgd@users.noreply.github.com>
    Co-authored-by: Pepe <pgarciag93@gmail.com>

--- lib/Resource/OrganizationMembership.php
+++ lib/Resource/OrganizationMembership.php
@@ -4,8 +4,16 @@ namespace WorkOS\Resource;
 
 /**
  * Class OrganizationMembership.
+ *
+ * @property 'organization_membership' $object
+ * @property string $id
+ * @property string $userId
+ * @property string $organizationId
+ * @property RoleResponse $role
+ * @property 'active'|'inactive'|'pending' $status
+ * @property string $createdAt
+ * @property string $updatedAt
  */
-
 class OrganizationMembership extends BaseWorkOSResource
 {
     public const RESOURCE_TYPE = "organization_membership";
@@ -15,6 +23,7 @@ class OrganizationMembership extends BaseWorkOSResource
         "id",
         "userId",
         "organizationId",
+        "role",
         "status",
         "createdAt",
         "updatedAt"
@@ -25,6 +34,7 @@ class OrganizationMembership extends BaseWorkOSResource
         "id" => "id",
         "user_id" => "userId",
         "organization_id" => "organizationId",
+        "role" => "role",
         "status" => "status",
         "created_at" => "createdAt",
         "updated_at" => "updatedAt"
