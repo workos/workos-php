a89434e372cedce8bd36a494fd25f4f77a14c137
Nick Nisi <nick.nisi@workos.com>
2025-07-07 10:29:14 -0500

    formatting

--- tests/WorkOS/WorkOSTest.php
+++ tests/WorkOS/WorkOSTest.php
@@ -10,24 +10,24 @@ class WorkOSTest extends TestCase
     {
         WorkOS::setApiKey(null);
         WorkOS::setClientId(null);
-        
+
         putenv("WORKOS_API_KEY=");
         putenv("WORKOS_CLIENT_ID=");
-        
+
         unset($_ENV['WORKOS_API_KEY']);
         unset($_ENV['WORKOS_CLIENT_ID']);
         unset($_SERVER['WORKOS_API_KEY']);
         unset($_SERVER['WORKOS_CLIENT_ID']);
     }
-    
+
     protected function tearDown(): void
     {
         WorkOS::setApiKey(null);
         WorkOS::setClientId(null);
-        
+
         putenv("WORKOS_API_KEY=");
         putenv("WORKOS_CLIENT_ID=");
-        
+
         unset($_ENV['WORKOS_API_KEY']);
         unset($_ENV['WORKOS_CLIENT_ID']);
         unset($_SERVER['WORKOS_API_KEY']);
@@ -37,14 +37,14 @@ class WorkOSTest extends TestCase
     public function testGetApiKeyFromEnvSuperglobal()
     {
         $_ENV['WORKOS_API_KEY'] = "pk_test_env_superglobal";
-        
+
         $this->assertEquals("pk_test_env_superglobal", WorkOS::getApiKey());
     }
 
     public function testGetClientIdFromEnvSuperglobal()
     {
         $_ENV['WORKOS_CLIENT_ID'] = "client_test_env_superglobal";
-        
+
         $this->assertEquals("client_test_env_superglobal", WorkOS::getClientId());
     }
 
@@ -52,7 +52,7 @@ class WorkOSTest extends TestCase
     {
         $_ENV['WORKOS_API_KEY'] = "pk_test_laravel_cached";
         $_ENV['WORKOS_CLIENT_ID'] = "client_test_laravel_cached";
-        
+
         $this->assertEquals("pk_test_laravel_cached", WorkOS::getApiKey());
         $this->assertEquals("client_test_laravel_cached", WorkOS::getClientId());
     }
