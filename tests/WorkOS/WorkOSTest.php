6fcfacd1bc49efece53477a3c9842b59b3ed8300
Nick Nisi <nick.nisi@workos.com>
2025-07-11 09:24:24 -0500

    Add getEnvVariable utility method to fix Laravel config caching issue (#293)
    * create a `getEnvVariable` utility and use it
    
    * add/update tests
    
    * clean up tests
    
    * formatting
    
    * Update lib/WorkOS.php
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

---
+++ tests/WorkOS/WorkOSTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace WorkOS;
+
+use PHPUnit\Framework\TestCase;
+
+class WorkOSTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        WorkOS::setApiKey(null);
+        WorkOS::setClientId(null);
+
+        putenv("WORKOS_API_KEY=");
+        putenv("WORKOS_CLIENT_ID=");
+
+        unset($_ENV['WORKOS_API_KEY']);
+        unset($_ENV['WORKOS_CLIENT_ID']);
+        unset($_SERVER['WORKOS_API_KEY']);
+        unset($_SERVER['WORKOS_CLIENT_ID']);
+    }
+
+    protected function tearDown(): void
+    {
+        WorkOS::setApiKey(null);
+        WorkOS::setClientId(null);
+
+        putenv("WORKOS_API_KEY=");
+        putenv("WORKOS_CLIENT_ID=");
+
+        unset($_ENV['WORKOS_API_KEY']);
+        unset($_ENV['WORKOS_CLIENT_ID']);
+        unset($_SERVER['WORKOS_API_KEY']);
+        unset($_SERVER['WORKOS_CLIENT_ID']);
+    }
+
+    public function testGetApiKeyFromEnvSuperglobal()
+    {
+        $_ENV['WORKOS_API_KEY'] = "pk_test_env_superglobal";
+
+        $this->assertEquals("pk_test_env_superglobal", WorkOS::getApiKey());
+    }
+
+    public function testGetClientIdFromEnvSuperglobal()
+    {
+        $_ENV['WORKOS_CLIENT_ID'] = "client_test_env_superglobal";
+
+        $this->assertEquals("client_test_env_superglobal", WorkOS::getClientId());
+    }
+
+    public function testLaravelConfigCachingScenario()
+    {
+        $_ENV['WORKOS_API_KEY'] = "pk_test_laravel_cached";
+        $_ENV['WORKOS_CLIENT_ID'] = "client_test_laravel_cached";
+
+        $this->assertEquals("pk_test_laravel_cached", WorkOS::getApiKey());
+        $this->assertEquals("client_test_laravel_cached", WorkOS::getClientId());
+    }
+}
